7651580f9457395b205ccfc384cc5e26b8545e3a
Peter Stevens <Peterstevens510@gmail.com>
2020-07-16 13:22:35 +0100

    Updates

---
+++ precache-manifest.56f215e2766f07e0f726409f4663d377.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf3902a1309af97dceeaf5084af40cf",
+    "url": "/CalculatorReact/index.html"
+  },
+  {
+    "revision": "09fec6da88904695ad0f",
+    "url": "/CalculatorReact/static/css/main.5f82e858.chunk.css"
+  },
+  {
+    "revision": "55faaa923751c98a0129",
+    "url": "/CalculatorReact/static/js/2.b089ea63.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/CalculatorReact/static/js/2.b089ea63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09fec6da88904695ad0f",
+    "url": "/CalculatorReact/static/js/main.ad85d0a8.chunk.js"
+  },
+  {
+    "revision": "d80c2d398c795a1f91b6",
+    "url": "/CalculatorReact/static/js/runtime-main.3f7eb7fb.js"
+  }
+]);
